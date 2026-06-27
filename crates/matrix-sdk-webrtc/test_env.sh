aa0df34dad4c420ed6c3f9faca90f9821ffae521
NGDH Deploy <ngdh@deploy.local>
2026-06-27 05:51:33 +0000

    upload project

--- crates/matrix-sdk-webrtc/test_env.sh
+++
@@ -1,8 +0,0 @@
-﻿
-export MATRIX_HOMESERVER="http://localhost:8008"
-export MATRIX_ROOM_ID="!wCPEWLklAfFASeXTpS:test.local"
-export ALICE_USER="@alice031054:test.local"
-export ALICE_PASS="test1234"
-export BOB_USER="@bob031054:test.local"
-export BOB_PASS="test1234"
-export RUST_LOG="two_party_media=info,matrix_sdk_webrtc=info"
